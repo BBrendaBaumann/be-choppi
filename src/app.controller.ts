3d94d60792272e14957c0a79e2b1214d3747da82
BBrendaBaumann <brenbaumann40@gmail.com>
2025-11-04 20:04:50 -0300

    mensaje bienvenida

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,16 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getWelcome(): object {
+    return {
+      message: '¡Bienvenido a Choppi API!',
+      endpoints: {
+        auth: '/auth',
+        stores: '/stores',
+        products: '/products',
+        cart: '/cart'
+      }
+    };
   }
-}
+  }
+
